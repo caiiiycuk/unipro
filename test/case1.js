ea28602650914fcbe43a00f51afe8c24e9ad0ccb
Aleksander Guryanov <caiiiycuk@gmail.com>
2017-08-10 13:57:08 +0700

    Add test/case1.js

---
+++ test/case1.js
@@ -0,0 +1,13 @@
+function a() {
+  var a = 0;
+  var b = 2;
+  if (b == 0) {
+    function z(a) {
+      if (a == 7)return 2;
+      return 4;
+    }
+    return z(a);
+  } else {
+    return b;
+  }
+}
